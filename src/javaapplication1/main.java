fdae7368fbd6ae244c73a7485c568772c15bbe95
bruno <bcgonzalez4@esei.uvigo.es>
2018-07-01 20:29:02 +0200

    Filter.tk

---
+++ src/javaapplication1/main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author bruno
+ */
+public class main {
+    
+    public static void main(String[] args){
+        /*Make the call to MenuFrame, which opens the main menu.*/
+        MenuFrame v = new MenuFrame();
+        v.main(args);
+        
+    }
+    
+}
